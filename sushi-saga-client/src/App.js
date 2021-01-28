9c1c2811a5c6697c6247e02b285f34359bc5c575
jackmt9 <jackmt9@gmail.com>
2021-01-28 15:53:27 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,46 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    eaten: [],
+    displayIndex: 0,
+    money: '100'
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(r => r.json())
+    .then(sushis => this.setState({sushis: sushis}))
+  }
+
+  getNextFour = () => {
+    this.setState(prevState => {
+      return {
+        displayIndex: prevState.displayIndex + 4
+      }
+    })
+  }
+
+  removeSushi = (sushi) => {
+    if(sushi.price > this.state.money){
+      alert("You can not afford this sushi.")
+      return false
+    }
+    this.setState(prevState => {
+      return {
+        sushis: prevState.sushis.filter(singleSushi => singleSushi !== sushi),
+        eaten: [...prevState.eaten, sushi],
+        money: prevState.money - sushi.price
+      }
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} displayIndex={this.state.displayIndex} getNextFour={this.getNextFour} removeSushi={sushi => this.removeSushi(sushi)} eaten={this.state.eaten}/>
+        <Table eaten={this.state.eaten} money={this.state.money}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,18 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+  // debugger
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushis.slice(props.displayIndex, props.displayIndex + 4).map(sushi => {
+            return <Sushi sushi={sushi} removeSushi={props.removeSushi} eaten={props.eaten.includes(sushi)} key={sushi.id}/>
+          })
         }
-        <MoreButton />
+        <MoreButton getNextFour={props.getNextFour}/>
       </div>
     </Fragment>
   )
